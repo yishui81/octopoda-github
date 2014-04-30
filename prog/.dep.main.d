2f6b3b3394c445e62a8a9b5b955b3c0290309050
jian.wang2 <jian.wang2@sihuatech.com>
2014-04-30 17:59:01 +0800

    remove some file

--- prog/.dep.main.d
+++
@@ -1,5 +0,0 @@
-main.o: main.cpp ../../SealsARE/BaseARE/ApplicationBase.h \
- ../../SealsARE/BaseARE/BaseLib.h ../../SealsARE/BaseARE/SfcLog.h \
- ../../SealsARE/BaseARE/UWEManager.h ../../SealsARE/BaseARE/UREData.h \
- ../../SealsARE/BaseARE/SockBase.h ../../SealsARE/BaseARE/UThread.h \
- ../../SealsARE/BaseARE/TimeValue.h ../../SealsARE/BaseARE/ConfigSet.h
